d8a9eb709fb689934f6ed8e7a3291f7ef411424b
Galoda4252 <saphiraroger08@gmail.com>
2024-04-02 11:27:00 +0200

    creation methode details

--- refactoring_tp_24/src/ui/bad/Humain.java
+++ refactoring_tp_24/src/ui/bad/Humain.java
@@ -2,4 +2,12 @@ package ui.bad;
 
 public class Humain {
 
+	private String nom;
+	private String prenom;
+	private int age;
+
+	 public void details() {
+	        System.out.println("Nom : " + nom+ "Prénom : " + prenom+"Âge : " + age);
+	       
+	    }
 }
