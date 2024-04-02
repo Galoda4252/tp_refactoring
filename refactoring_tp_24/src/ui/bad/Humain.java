c4b09759f2f99b1e5bf678d121eead6408880680
Galoda4252 <saphiraroger08@gmail.com>
2024-04-02 11:29:34 +0200

    setter et getter

--- refactoring_tp_24/src/ui/bad/Humain.java
+++ refactoring_tp_24/src/ui/bad/Humain.java
@@ -6,7 +6,31 @@ public class Humain {
 	private String prenom;
 	private int age;
 
-	 public void details() {
+	 public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void details() {
 	        System.out.println("Nom : " + nom+ "Prénom : " + prenom+"Âge : " + age);
 	       
 	    }
